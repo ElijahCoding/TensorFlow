9cbd068f046023ac09d41023d86072f3b31c9ddd
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-11 17:28:16 +0300

    tensorflow mnist fashion finished

---
+++ Minist.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(test_images)
